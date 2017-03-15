pohly/meta-swupd

commit c63e8c0d95604ee5e4220cb093a3c5ab7c60c0b5

swupd-image.bbclass: don't fail parsing if SWUPD_BUNDLES is undefined
ERROR: Error for ...image.bb, dependency ${@' does not contain exactly one ':' character.
 Task 'depends' should be specified in the form 'packagename:task'
ERROR: Command execution failed: Exited with 1

Signed-off-by: André Draszik <git@andred.net>

classes/swupd-image.bbclass
@@ -249,7 +249,7 @@ addtask stage_swupd_inputs after do_swupd_list_bundle before do_swupd_update
 do_stage_swupd_inputs[dirs] = "${SWUPDIMAGEDIR} ${SWUPDMANIFESTDIR} ${DEPLOY_DIR_SWUPD}/maps/"
 do_stage_swupd_inputs[depends] += " \
     virtual/fakeroot-native:do_populate_sysroot \
-    ${@ ' '.join(['bundle-${SWUPD_IMAGE_PN}-%s:do_swupd_list_bundle' % x for x in '${SWUPD_BUNDLES}'.split()]) } \
+    ${@ ' '.join(['bundle-${SWUPD_IMAGE_PN}-%s:do_swupd_list_bundle' % x for x in (d.getVar('SWUPD_BUNDLES') or '').split()]) } \
 "
 
 python do_fetch_swupd_inputs () {

commit 1098ec6306284551ce17b69899ef085e339addb2

swupd-image.bbclass: add another missing expand arg in getVars
Add "True" to d.getVar() to avoid forking a little while longer.

This got broken in commit c63e8c0d95604ee5 "swupd-image.bbclass: dont fail parsing if SWUPD_BUNDLES is undefined"

Signed-off-by: Aaron Zinghini <aaron.zinghini@seeingmachines.com>

classes/swupd-image.bbclass
@@ -249,7 +249,7 @@ addtask stage_swupd_inputs after do_swupd_list_bundle before do_swupd_update
 do_stage_swupd_inputs[dirs] = "${SWUPDIMAGEDIR} ${SWUPDMANIFESTDIR} ${DEPLOY_DIR_SWUPD}/maps/"
 do_stage_swupd_inputs[depends] += " \
     virtual/fakeroot-native:do_populate_sysroot \
-    ${@ ' '.join(['bundle-${SWUPD_IMAGE_PN}-%s:do_swupd_list_bundle' % x for x in (d.getVar('SWUPD_BUNDLES') or '').split()]) } \
+    ${@ ' '.join(['bundle-${SWUPD_IMAGE_PN}-%s:do_swupd_list_bundle' % x for x in (d.getVar('SWUPD_BUNDLES', True) or '').split()]) } \
 "
 
 python do_fetch_swupd_inputs () {